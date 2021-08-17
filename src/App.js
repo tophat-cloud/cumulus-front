7c46f346665724a66c7a98b2fe820665abacc155
JeongHeon Woo <email@wookingwoo.com>
2021-08-18 02:12:18 +0900

    Add sign-in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hello from "./Hello";
 import Dashboard from "./dashboard/Dashboard";
+import SignIn from "./sign-in/SignIn";
 
 function App() {
   return (
     <div>
       {/* <Hello name="react" /> */}
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <SignIn />
     </div>
   );
 }
