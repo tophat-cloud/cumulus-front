6b189727af514ba7fa71ac2faa76056a17218e72
JeongHeon Woo <contact@wookingwoo.com>
2021-10-14 01:56:42 +0900

    Connect API: get project All

--- src/dashboard/selectProject.js
+++ src/dashboard/selectProject.js
@@ -1,4 +1,6 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import Box from "@material-ui/core/Box";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -6,14 +8,49 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
 export default function ProjectSelect() {
-  const [projectId, setProjectId] = React.useState("");
+  //   const [projectId, setProjectId] = React.useState("");
+
+  //   ddd
+
+  const [projectsList, setProjectsList] = useState([]);
+  const [selectedProjectId, setSelectedProjectId] = useState("초기값..");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        // 요청이 시작 할 때에는 error 와 setProjectsList를 초기화
+        setError(null);
+        setProjectsList([]);
+        // loading 상태를 true로 설정
+        setLoading(true);
+        const response = await axios.get(
+          "https://api.cumulus.tophat.cloud/project"
+        );
+        setProjectsList(response.data); // 데이터는 response.data 안에 들어있다.
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchProjects();
+  }, []);
+
+  //   console.log(projectsList);
+
+  // ddd
 
   const handleChange = (event) => {
     if (event.target.value === "AddProject") {
       alert("Add project!");
-      setProjectId("");
+      setSelectedProjectId(null);
     } else {
-      setProjectId(event.target.value);
+      alert(event.target.value);
+      setSelectedProjectId(event.target.value);
+      console.log(selectedProjectId);
+      alert(selectedProjectId);
     }
   };
 
@@ -24,13 +61,14 @@ export default function ProjectSelect() {
         <Select
           labelId="select-prject"
           id="select-prject"
-          value={projectId}
+          //   value={selectedProjectId}
           label="select-prject"
           onChange={handleChange}
         >
-          <MenuItem value={10}>Project1</MenuItem>
-          <MenuItem value={20}>Project2</MenuItem>
-          <MenuItem value={30}>Project3</MenuItem>
+          {projectsList.map((projects) => (
+            <MenuItem value={projects.id}> {projects.title} </MenuItem>
+          ))}
+
           <MenuItem value={"AddProject"}>Add New Project</MenuItem>
         </Select>
       </FormControl>
